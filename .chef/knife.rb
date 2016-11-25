8de2247eb0db11f77dcd027e05833ba27bc6c16d
Bogdan Muţiu <bmutziu@gmail.com>
2016-11-25 17:37:35 +0200

    The Book Env

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -12,3 +12,7 @@ cookbook_path	["#{current_dir}/../cookbooks"]
 cookbook_copyright "Fidelia People's Card"
 cookbook_license "apachev2"
 cookbook_email "bogdan@vdg.io"
+knife[:azure_subscription_id] = "5d97deb7-8db6-47ed-b008-e49c940408fe"
+knife[:azure_mgmt_cert] = "/home/bmutziu/Public/management-certificate.pem"
+#knife[:azure_api_host_name] = "management-preview.core.windows-int.net"
+knife[:azure_api_host_name] = "https://management.core.windows.net"
